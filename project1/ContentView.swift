ee7685ad146f8b0ab3ae9b5452b22219a576042b
dikodiko01 <dikodiko901123@gmail.com>
2022-11-16 16:23:59 +0600

    Initial Commit

---
+++ project1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project1
+//
+//  Created by Diar Orynbek on 16.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
